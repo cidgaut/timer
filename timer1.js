a6f695bf2f298b553f0cde490ff97b29c39acd11
Cedric.gauthier <cidgaut@gmail.com>
2024-01-16 16:16:55 -0500

    eslint

--- timer1.js
+++ timer1.js
@@ -1,18 +1,18 @@
-//edge case: no input given 
+//edge case: no input given
 if (process.argv.length <= 2) {
   console.log("No numbers provided.");
   process.exit(1);
 }
 
-//edge case: number is negative //edge case: input is not a number 
+//edge case: number is negative //edge case: input is not a number
 for (let i = 2; i < process.argv.length; i++) {
   const number = process.argv[i];
 
   if (isNaN(number) || number < 0) {
-    console.log(`${process.argv[i]} invalid`)
+    console.log(`${process.argv[i]} invalid`);
   }
 
   setTimeout(() => {
     process.stdout.write('\x07');
   }, number * 1000);  //make input in milliseconds convert to seconds.
-};
+}
