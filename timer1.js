9e27a7224334eee82faf71af28efdeb92331d920
Cedric.gauthier <cidgaut@gmail.com>
2024-01-16 16:15:32 -0500

    system works. converted input from command line conversion to seconds

--- timer1.js
+++ timer1.js
@@ -1,10 +1,3 @@
-process.stdout.write('\x07');
-
-//make alarm sound when command line prompts are given (unlimited)
-//EX: node timer1.js 10 3 5 15 9 
-
-//command line prompt seen in week 1 (node file.js are the first two in precess.argv.length)
-
 //edge case: no input given 
 if (process.argv.length <= 2) {
   console.log("No numbers provided.");
@@ -21,15 +14,5 @@ for (let i = 2; i < process.argv.length; i++) {
 
   setTimeout(() => {
     process.stdout.write('\x07');
-  }, number);
-
+  }, number * 1000);  //make input in milliseconds convert to seconds.
 };
-
-/* FOR REFERRENCE
-for (const char of spinner) {
-  setTimeout(() => {
-    process.stdout.write(`\r${char}   `);
-  }, delay);
-  delay += 200;
-}*/
-
